51a6018458cf7ef1203bf46880b196dc9a237965
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-11-27 13:36:32 +0700

    redux init

---
+++ src/reducers/auth.js
@@ -0,0 +1,18 @@
+import { ActionType } from '../constants/ActionType'
+
+const initialState = {
+  user: {
+    username: 'duydien',
+    email: 'duydien552@gmail.com'
+  }
+}
+
+export default function AuthReducers(state = initialState, action) {
+  switch (action.type) {
+    case ActionType.LOGIN:
+      return state
+
+    default:
+      return state
+  }
+}
