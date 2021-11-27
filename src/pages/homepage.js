a273c155ddff7a8342b2a1931552c19eda582692
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2021-11-27 21:47:23 +0700

    resolve key bug

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,8 +1,8 @@
 import Navbar from '../components/Common/Navbar/Navbar'
-import TopSlider from '../components/Homepage/TopSlider/TopSlider'
-import TopProducts from '../components/Homepage/TopProducts/TopProducts'
+import TopSlider from '../components/HomePage/TopSlider/TopSlider'
+import TopProducts from '../components/HomePage/TopProducts/TopProducts'
 import React from 'react'
-import HotDeals from '../components/Homepage/HotDeals/HotDeals'
+import HotDeals from '../components/HomePage/HotDeals/HotDeals'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Footer from '../components/Common/Footer/Footer'
 function HomePage() {
