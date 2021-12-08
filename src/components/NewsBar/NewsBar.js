ebd628c45ecc13b9541bba25c39f5179c8df405b
duydien0920 <duydien552@gmail.com>
2021-12-09 00:11:41 +0700

    fix/newsbar

---
+++ src/components/NewsBar/NewsBar.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import defaultNewsImage from '../../assets/defaultNewsImage.png'
+import './NewsBar.css'
+
+function NewsBar() {
+  return (
+    <div className="NewsBar">
+      <div className="container-NewsBar">
+        <div className="container-NewsBar-img">
+          <img src={defaultNewsImage} alt="news" />
+        </div>
+        <div>
+          <span>
+            Có nên mua đồng hồ theo dõi sức khỏe không? Những lý do sau đây sẽ khiến bạn phải chốt ngay một 'em' đồng hồ
+            đấy! 5 ngày trước
+          </span>
+        </div>
+      </div>
+      <div className="container-NewsBar">
+        <div className="container-NewsBar-img">
+          <img src={defaultNewsImage} alt="news" />
+        </div>
+        <div>
+          <span>
+            Có nên mua đồng hồ theo dõi sức khỏe không? Những lý do sau đây sẽ khiến bạn phải chốt ngay một 'em' đồng hồ
+            đấy! 5 ngày trước
+          </span>
+        </div>
+      </div>
+      <div className="container-NewsBar">
+        <div className="container-NewsBar-img">
+          <img src={defaultNewsImage} alt="news" />
+        </div>
+        <div>
+          <span>
+            Có nên mua đồng hồ theo dõi sức khỏe không? Những lý do sau đây sẽ khiến bạn phải chốt ngay một 'em' đồng hồ
+            đấy! 5 ngày trước
+          </span>
+        </div>
+      </div>
+      <div className="container-NewsBar">
+        <div className="container-NewsBar-img">
+          <img src={defaultNewsImage} alt="news" />
+        </div>
+        <div>
+          <span>
+            Có nên mua đồng hồ theo dõi sức khỏe không? Những lý do sau đây sẽ khiến bạn phải chốt ngay một 'em' đồng hồ
+            đấy! 5 ngày trước
+          </span>
+        </div>
+      </div>
+      <div className="container-NewsBar">
+        <div className="container-NewsBar-img">
+          <img src={defaultNewsImage} alt="news" />
+        </div>
+        <div>
+          <span>
+            Có nên mua đồng hồ theo dõi sức khỏe không? Những lý do sau đây sẽ khiến bạn phải chốt ngay một 'em' đồng hồ
+            đấy! 5 ngày trước
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewsBar
